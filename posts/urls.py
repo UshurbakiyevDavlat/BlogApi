29c7336a608ac4b935345822b6cb3dad4f6cce37
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-05 14:48:49 +0600

    added permissions

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from posts.views import PostDetail, PostList
 
 urlpatterns = [
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list")
+    path("", PostList.as_view(), name="post_list"),
+    path("api-auth/", include('rest_framework.urls')),
 ]
