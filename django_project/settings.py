2c75177d543cd4230958db5b4b5bced9ab96ddf2
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-05 16:45:00 +0600

    added registration, allAuth package, and updated passwrod reset and confirm

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",  # new
     "rest_framework.authtoken",  # new
+    "allauth",  # new
+    "allauth.account",  # new
+    "allauth.socialaccount",  # new
     "dj_rest_auth",  # new
+    "dj_rest_auth.registration",  # new
     # Local
     "accounts.apps.AccountsConfig",  # new
     "posts.apps.PostsConfig",  # new
@@ -53,7 +57,7 @@ REST_FRAMEWORK = {
     ],
 
     "DEFAULT_AUTHENTICATION_CLASSES": [  # new
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication"
     ],
 }
@@ -89,10 +93,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request",  # new
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # new
+SITE_ID = 1  # new
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
