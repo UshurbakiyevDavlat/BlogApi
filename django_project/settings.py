e7c514a8ec3d0296582f94cbcb9e73feaa6cfb59
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-09 14:58:59 +0600

    added limitOffsetPagination

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,6 +58,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated"  # new
     ],
